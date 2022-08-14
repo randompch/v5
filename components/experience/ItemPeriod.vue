2d06f080cdf7e8ee0187eba90a1e412453cc15bd
randompch <antoinepiche@gmail.com>
2022-08-14 13:00:11 +0200

    feat(comp): create ExperienceItemPeriod component

---
+++ components/experience/ItemPeriod.vue
@@ -0,0 +1,30 @@
+<template>
+  <span
+    v-if="hasPeriod || experience.timeSpent"
+    class="flex flex-col items-end mt-0.5 font-mono text-xxs md:text-sm"
+  >
+    <span v-if="hasPeriod">
+      {{ experience.startedAt }} - {{ experience.endedAt }}
+    </span>
+    <span
+      v-if="experience.timeSpent"
+      class="text-zinc-400"
+    >
+      {{ experience.timeSpent }}
+    </span>
+  </span>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+import { Experience } from '@/types/interfaces/Experience.interface';
+
+const props = defineProps<{
+  experience: Experience;
+}>();
+
+const hasPeriod = computed(() => {
+  return !!props.experience?.startedAt && !!props.experience?.endedAt;
+});
+</script>
