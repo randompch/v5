9fbe0f4f7eac953bfcf9a88537d2df1dc9348c9d
randompch <antoinepiche@gmail.com>
2022-08-14 13:00:49 +0200

    feat(comp): create ExperienceItems component

---
+++ components/experience/Items.vue
@@ -0,0 +1,23 @@
+<template>
+  <ul class="w-full mt-4 space-y-6">
+    <ExperienceItem
+      v-for="(experience, index) in experiences"
+      :key="index"
+      :experience="experience"
+      :is-child="isChild"
+      :is-last="index === experiences.length - 1"
+    />
+  </ul>
+</template>
+<script setup lang="ts">
+defineProps({
+  experiences: {
+    type: Array,
+    required: true,
+  },
+  isChild: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
