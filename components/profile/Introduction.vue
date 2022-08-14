39e37c695043355d7be84d1530d68b68847ceb67
randompch <antoinepiche@gmail.com>
2022-08-14 12:56:41 +0200

    feat(comp): create ProfileIntroduction component

---
+++ components/profile/Introduction.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="w-full">
+    <h1 class="text-3xl font-bold text-zinc-700 dark:text-white">
+      Hi there! 👋 I'm Antoine<span class="text-emerald-450">,</span>
+    </h1>
+    <p class="mt-3 font-normal">
+      a Front-end engineer <br class="block md:hidden">& Vue.js enthusiast based in Paris.
+      <span class="text-zinc-400 dark:text-zinc-400">
+        <br class="hidden md:block">currently working
+        <BaseLink
+          alt="Selency website"
+          href="https://selency.com"
+          is-primary
+        >
+          @Selency
+        </BaseLink>
+        <br> with Nuxt, Node and Go.
+      </span>
+    </p>
+  </div>
+</template>
